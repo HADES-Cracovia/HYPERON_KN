d73c5260282193380538f96c5b88d7dca9e4c9d4
krzysn <k.nowakowski@doctoral.uj.edu.pl>
2019-07-24 16:09:22 +0200

    add missing files

---
+++ Lambda1520/LambdaEpEm/profile_jk.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+unset MYHADIR
+export MYHADDIR=/lustre/nyx/hades/user/jkubos/hades/pp45/install
+
+#. /cvmfs/hades.gsi.de/install/5.34.34/old/hydra-11042016/defall.sh
+. /lustre/nyx/hades/user/rlalik/hades/software/scripts/initHydra.sh fwdet
+
+export ORACLE_HOME=/cvmfs/it.gsi.de/oracle/product/12.1.2/client_x86_64_1
+export CERN_ROOT=/cvmfs/hades.gsi.de/install/cernlib_gfortran/2005/
+
